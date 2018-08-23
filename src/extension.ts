d2cadfed1c29ee224d7ef27b7474e2a8478e5e15
Nikola Zaric <nikola.zaric@sap.com>
2018-08-23 11:54:00 -0400

    #10 - removed onDidSave event and implemented command-only activation

--- src/extension.ts
+++ src/extension.ts
@@ -8,19 +8,20 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
   console.log(`sort imports is active!`);
 
-  const editor = vscode.window.activeTextEditor;
-  if (!editor) {
-    return; // No open text editor
-  }
+  const disposable = vscode.commands.registerCommand('extension.sortImports', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return; // No open text editor
+    }
 
-  vscode.workspace.onDidSaveTextDocument(event => {
-    if (!isTypeScriptFile(event.languageId)) {
+    if (!isTypeScriptFile(editor.document.languageId)) {
       return;
     }
 
-    const imports = getImports(event);
+    const imports = getImports(editor.document);
     console.log(`imports`, imports);
   });
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
