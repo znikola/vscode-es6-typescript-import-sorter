42a40558e6f22274a0db391a6c917f352922e440
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2018-09-07 18:10:16 -0400

    fixed file writting

--- src/file-writer.ts
+++ src/file-writer.ts
@@ -1,13 +1,37 @@
 // Utility file to write the imports to the document
 import * as vscode from 'vscode';
 
-import { Import } from './models/import';
+import { ImportGroup } from './models/import-group';
 
-export function fileWriterUtil(editor: vscode.TextDocument, newImports: Import[]): Thenable<boolean> {
+export function fileWriterUtil(editor: vscode.TextDocument, newImports: ImportGroup[]): Thenable<boolean> {
   const workspaceEdits = new vscode.WorkspaceEdit();
-  newImports.forEach(value => {
-    workspaceEdits.replace(editor.uri, new vscode.Range(value.startPosition, value.endPosition), value.statement);
-  });
+
+  for (let i = 0; i < newImports.length; i++) {
+    for (let j = 0; j < newImports[i].imports.length; j++) {
+      const startPosition = new vscode.Position(
+        newImports[i].imports[j].startPosition.line,
+        newImports[i].imports[j].startPosition.character
+      );
+      const endPosition = new vscode.Position(
+        newImports[i].imports[j].endPosition.line,
+        newImports[i].imports[j].endPosition.character
+      );
+      workspaceEdits.replace(
+        editor.uri,
+        new vscode.Range(startPosition, endPosition),
+        newImports[i].imports[j].statement
+      );
+    }
+
+    if (i !== newImports.length - 1) {
+      const startPosition = new vscode.Position(
+        newImports[i].imports[newImports[i].imports.length - 1].startPosition.line,
+        newImports[i].imports[newImports[i].imports.length - 1].endPosition.character + 1
+      );
+      const endPosition = new vscode.Position(startPosition.line, startPosition.character + 2);
+      workspaceEdits.replace(editor.uri, new vscode.Range(startPosition, endPosition), '\n');
+    }
+  }
 
   return textWriter(workspaceEdits);
 }
--- src/group-imports.ts
+++ src/group-imports.ts
@@ -1,7 +1,14 @@
-import { Import } from './models/import';
+'use strict';
 
-export function groupImports(imports: Import[]): Import[] {
-  let last: string;
+import * as vscode from 'vscode';
+
+import { Import } from './models/import';
+import { ImportGroup } from './models/import-group';
+
+export function groupImports(imports: Import[]): ImportGroup[] {
+  let last = '';
+  const importGroups: ImportGroup[] = [];
+  let currentImports: Import[] = [];
 
   for (let i = 0; i < imports.length; i++) {
     const statement = imports[i].from;
@@ -9,10 +16,23 @@ export function groupImports(imports: Import[]): Import[] {
 
     if (last) {
       if (last !== current[0]) {
-        imports[i - 1].statement = imports[i - 1].statement.concat('\n');
+        const blankLinePostion = new vscode.Position(currentImports[currentImports.length - 1].endPosition.line + 1, 1);
+        importGroups.push({ imports: currentImports, blankLinePostion });
+        currentImports = [];
+        currentImports.push(imports[i]);
+      } else {
+        currentImports.push(imports[i]);
       }
+    } else {
+      currentImports.push(imports[i]);
     }
+
     last = current[0];
+
+    if (i === imports.length - 1) {
+      const blankLinePostion = new vscode.Position(currentImports[currentImports.length - 1].endPosition.line + 1, 1);
+      importGroups.push({ imports: currentImports, blankLinePostion });
+    }
   }
-  return imports;
+  return importGroups;
 }
